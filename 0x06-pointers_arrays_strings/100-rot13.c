7b05a95549d7e0e54e5809c4fd997897801418b5
Ahmed Fouad LAGHA <107846932+ahmedfouadlagha@users.noreply.github.com>
2023-07-27 15:07:39 +0100

    Update 100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,31 +1,25 @@
 #include "main.h"
+
 /**
- *rot13 - encodes strings using rot13.
- *@s: pointer to string.
+ * rot13 - encodes a string using rot13
+ * @s: the string
  *
- *Return: pointer to encoded string.
+ * Return: the encoded string
  */
+
 char *rot13(char *s)
 {
-	int stringCount, rotation;
-	char r1[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
-		     'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
-		     'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
-		     'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y',
-		     'Z'};
-	char r2[] = {'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
-		     'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
-		     'm', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y',
-		     'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
-		     'M'};
+	int i, j;
+	char *letters1 = {"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghigklmnopqrstuvwxyz"};
+	char *letters2 = {"NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm"};
 
-	for (stringCount = 0; s[stringCount] != '\0'; stringCount++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (rotation = 0; rotation < 53; rotation++)
+		for (j = 0; j < 52; j++)
 		{
-			if (r1[rotation] == s[stringCount])
+			if (s[i] == letters1[j])
 			{
-				s[stringCount] = r2[rotation];
+				s[i] = letters2[j];
 				break;
 			}
 		}
