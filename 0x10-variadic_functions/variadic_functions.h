aed9b2469c454f040f9223c622b01d30ae901373
ahmedfouadlagha <laghaahmedfouad@gmail.com>
2023-08-17 19:05:50 +0100

    0x10

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -9,7 +9,7 @@ char *symbol;
 void (*print)(va_list arg);
 } printer_t;
 
-int _putchar(char c);
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
