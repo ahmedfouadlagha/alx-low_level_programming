f58157ca8db05b3d7c1b6f82b6cf3f8b39a3a1b8
ahmedfouadlagha <laghaahmedfouad@gmail.com>
2023-08-30 00:53:10 +0100

    changed

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -18,6 +18,11 @@ int n;
 struct listint_s *next;
 } listint_t;
 
+typedef struct listp_s
+{
+	void *p;
+	struct listp_s *next;
+} listp_t;
 
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
